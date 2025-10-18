9063fee8c88ece514b73639ee759e6788943043c
Leandro Monteiro <leandro.a.monteiro1@gmail.com>
2025-10-18 20:07:53 -0300

    Checkbox | forEach | listar metas | marcar e desmarcar informação

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,22 +1,48 @@
-const { select , input } = require ('@inquirer/prompts')
+const { select , input , checkbox} = require ('@inquirer/prompts')
 
-let meta = {
-    value: 'Tomar 3L de água por dia',
-    checked: false,
+let fazer = {
+    value: 'Tomar 3 litros de agua todos os dias.',
+    checked: false
 }
 
-let metas = [meta ]
+let metas = [fazer]
 
 const cadastraMeta = async () =>{
-    const meta = await input ({ message: 'Digite a meta:'})
+    const fazer = await input ({ message: 'Digite a meta:'})
 
-    if(meta.length == 0) {
+    if(fazer.length == 0) {
         console.log('A meta não pode ser vazia.')
         return
     }
 
     metas.push(
-        { value: meta , checked: false})
+        { value: fazer , checked: false })
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox ({
+        message: 'Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa',
+        choices: [...metas] , 
+        instructions : false
+    })
+
+    if(respostas.length ==0) {
+        console.log('Nenhuma meta selecionada.')
+        return
+    }
+     
+    metas.forEach((m) => {
+    m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const fazer = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        fazer.checked = true
+    })
+    console.log('Meta(s) concluida(s).')
 }
 
 const start = async () => {
@@ -48,7 +74,7 @@ const start = async () => {
                         console.log(metas)
                         break
                     case 'listar':
-                        console.log('vamos listar')
+                        await listarMetas()
                         break
                     case 'sair':
                         console.log('Até logo')
@@ -57,5 +83,6 @@ const start = async () => {
         }
     }        
 }
+
 
 start()
